cf01e14bcb31c6fd90e7faf82b356f37ca97f57f
R9_ <neamtrares@naver.com>
2024-06-04 21:58:21 +0300

    Simple GUI

--- DN Interpreter/interpreter.py
+++ DN Interpreter/interpreter.py
@@ -1,5 +1,8 @@
+import PySimpleGUI as sg
+
 class Interpreter:
     def __init__(self, filename="main.dn"):
+        self.layout = []
         self.filename = filename
         with open(filename, "r+") as f:
             self.code = f.readlines()
@@ -9,6 +12,7 @@ class Interpreter:
         if not self.code[0] == "START":
             self = None
         self.index = 0
+        self.win_title = "App"
     
     def interpret(self, line):
         args = line.split(" ")
@@ -19,12 +23,23 @@ class Interpreter:
             print(" ".join(args))
         elif start.lower() == "title":
             print("Set title to", " ".join(args))
+            self.win_title = " ".join(args)
+        elif start.lower() == "text":
+            self.layout.append([sg.Text(" ".join(args))])
         else:
             print(start+": Command not found")
     
     def start_interpreter(self):
         for self.index in range(len(self.code)):
             self.interpret(self.code[self.index])
+        self.window = sg.Window(self.win_title, self.layout)
+        while True:
+            event, values = self.window.read()
+
+            # if user closes window or clicks cancel
+            if event == sg.WIN_CLOSED or event == 'Cancel':
+                break
+
 
 if __name__ == "__main__":
     interp = Interpreter("tmp.dn")
